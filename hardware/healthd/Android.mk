c341ff2fd65cb3c3a11df3d2e710ae645cdbb3b1
Alfred Neumayer <dev.beidl@gmail.com>
2018-08-27 23:44:13 +0200

    healthd: avoid building healthd on UBPorts configurations

--- hardware/healthd/Android.mk
+++ hardware/healthd/Android.mk
@@ -1,12 +1,12 @@
-LOCAL_PATH := $(call my-dir)
+#LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
+#include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := healthd_board_sony.cpp
-LOCAL_MODULE := libhealthd.$(TARGET_DEVICE)
-LOCAL_CFLAGS := -Werror
-LOCAL_C_INCLUDES := \
-    system/core/healthd/include \
-    bootable/recovery
+#LOCAL_SRC_FILES := healthd_board_sony.cpp
+#LOCAL_MODULE := libhealthd.$(TARGET_DEVICE)
+#LOCAL_CFLAGS := -Werror
+#LOCAL_C_INCLUDES := \
+#    system/core/healthd/include \
+#    bootable/recovery
 
-include $(BUILD_STATIC_LIBRARY)
+#include $(BUILD_STATIC_LIBRARY)
