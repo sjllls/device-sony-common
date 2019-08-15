7a65255b65d4e69cf0efc8569470c7a256a06b94
Alfred E. Neumayer <dev.beidl@gmail.com>
2019-08-15 04:49:46 +0200

    qcom: Use msm8998 CAF Audio HAL

--- hardware/qcom/Android.mk
+++ hardware/qcom/Android.mk
@@ -8,7 +8,7 @@ MSM_VIDC_TARGET_LIST := msm8916 msm8952 msm8996 msm8998
 #List of targets that use master side content protection
 MASTER_SIDE_CP_TARGET_LIST := msm8996 msm8998
 
-audio-hal := hardware/qcom/audio
+audio-hal := hardware/qcom/audio-caf/msm8998
 gps-hal := hardware/qcom/gps/msm8994
 
 ifeq ($(TARGET_KERNEL_VERSION),3.18)
